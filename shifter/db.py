27b0a3318b5599b0bfd3c103cad9758705469c26
Ricardo Gamba <rgamba@gmail.com>
2016-10-27 18:57:58 -0500

    add eq and ne to classes

--- shifter/db.py
+++ shifter/db.py
@@ -3,6 +3,7 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 import sys
 import time
 import hashlib
+import copy
 
 import click
 from invoke import run
@@ -206,7 +207,19 @@ class Column(object):
         return self.kind == 'clustering'
 
     def dump_cql(self):
-        return '{} {}'.format(self.type, self.name)
+        return '{} {}'.format(self.name, self.type.upper())
+
+    def __eq__(self, other):
+        if not isinstance(other, Column):
+            return False
+        return (self.name == other.name and
+                self.type.lower() == other.type.lower() and
+                self.kind == other.kind and
+                self.order == other.order and
+                self.position == other.position)
+
+    def __ne__(self, other):
+        return not self.__eq__(other)
 
 
 class Table(object):
@@ -234,13 +247,35 @@ class Table(object):
             r.append(cl[i])
         return r
 
+    def get_column(self, name):
+        for c in self.columns:
+            if c.name == name:
+                return c
+        return None
+
     def dump_cql(self):
         cql = ['CREATE TABLE {} ('.format(self.name)]
-        cql.append('\t' + ',\n'.join([x.dump_cql() for x in self.columns]))
+        cql.append('\t' + ',\n\t'.join([x.dump_cql() for x in self.columns]))
         pk = self.primary_keys()
         pks = ('({})' if len(pk) > 1 else '{}').format(', '.join(pk))
         cc = self.clustering_columns()
-        clustering = ',' + ', '.join(cc) if cc else ''
+        clustering = ', ' + ', '.join(cc) if cc else ''
         cql.append('\tPRIMARY KEY ({}{})'.format(pks, clustering))
         cql.append(')')
         return '\n'.join(cql)
+
+    def __eq__(self, other):
+        if not isinstance(other, Table):
+            return False
+        if len(self.columns) != len(other.columns):
+            return False
+        for col in self.columns:
+            other_col = other.get_column(col.name)
+            if not other_col:
+                return False
+            if col != other_col:
+                return False
+        return True
+
+    def __ne__(self, other):
+        return not self.__eq__(other)
